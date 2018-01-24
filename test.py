08658ff93c0f89802ef240af21a9da27fc7f8af0
Sarath <insomaniac99@gmail.com>
2018-01-23 21:56:52 -0500

    this is my first commit message

--- test.py
+++ test.py
@@ -1,1 +1,24 @@
+
+from flask import Flask
+app = Flask(__name__)
+
+
 print("hello world")
+
+a = 1
+b = 2
+c = 'Sarath'
+d = "sarath"
+ 
+# print (a,b,c,d)
+# print (c+d)
+
+myList = [a, b, c, d, "sarath24"]
+
+for x in myList:
+    print (x)
+
+
+@app.route("/")
+def hello():
+    return "Hello World"
