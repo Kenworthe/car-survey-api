e7f5ebe1cf9e02c0501a2cafe8950eb97d099329
Kenneth Lin <kenworthe@gmail.com>
2018-02-07 22:05:52 -0500

    added fake seeds.json database; added logic to render json to html page

--- app.py
+++ app.py
@@ -1,8 +1,14 @@
 from flask import Flask
 from flask import render_template
+import json
+
 app = Flask(__name__)
 
 print("hello world")
+
+seeds = {}
+with open("seeds.json") as json_data:
+	seeds = json.load(json_data)
 
 
 # Example of a For Loop:
@@ -15,20 +21,17 @@ print("hello world")
 def hello():
 	# business logic goes here
 	
-	a = 1
-	b = 2
-	c = "Kenny1"
-	d = 'Homepage!!!'
+	# question = "Question from DB goes here!"
+	# choice_list = ["choice A", "choice B", "choice C", "choice Z"]
 
-	myList = [a, b, c, d, "Kenny3"]
+	question = seeds["question1"]["question"]
+	choice_list = seeds["question1"]["choices"]
 
-	# by default, Flask requires return a String
-	# goal is to learn how to return an .html file
-	return render_template('index.html', message = d)
+	return render_template('index.html', question = question, choice_list = choice_list)
 
 @app.route("/kenny")
 def kenny():
 	return "This is kenny's profile page."
 
 # Instructions on how to launch our Flask server: 
-# Type into Terminal:  FLASK_APP=hello.py flask run
+# Type into Terminal:  FLASK_APP=app.py flask run
