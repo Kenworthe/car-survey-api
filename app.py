2f7d1eca61d97d80b1a26cbb6582f9d1ffaba788
Kenneth Lin <kenworthe@gmail.com>
2018-07-20 00:06:31 -0400

    added Dockerfile and requirements.txt

--- app.py
+++ app.py
@@ -1,7 +1,5 @@
 from flask import Flask, render_template, request, jsonify
 from flask_pymongo import PyMongo
-from flask_graphql import GraphQLView
-import graphene
 from bson import json_util
 import json
 
