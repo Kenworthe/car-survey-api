b660d2489f08c791f66206ee23935cf5436582c2
Kenneth Lin <kenworthe@gmail.com>
2018-07-26 00:19:55 -0400

    removed clutter from app.py

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ from flask_pymongo import PyMongo
 from bson import json_util
 import json
 
-print("hello world")
+print('Hi! Running car-survey-api...')
 
 app = Flask(__name__)
 
@@ -31,14 +31,7 @@ def home():
 
 	if request.method == 'GET':
 
-		# results = list(mongo.db.questions.find({}))
-		# print(results)
-
-		# first = results[0]
-
-		# return json.dumps(results, default=json_util.default)
-		return 'hello, this is the home page!'
-		# return render_template('index.html', question = first["question"], choice_list = first["choices"])
+		return 'Hello, welcome to the car-survey API! Visit /api/questions or /api/cars for data.'
 
 	if request.method == 'POST':
 	
@@ -58,14 +51,14 @@ def home():
 		data = {}
 		return jsonify(data)
 
-@app.route("/questions", methods=['GET'])
+@app.route("/api/questions", methods=['GET'])
 def fetch_all_questions():
 	# results = list(mongo.db.questions.find({}))
 	# return json.dumps(results, default=json_util.default)
 	results = get_questions_from_seed()
 	return jsonify(results)
 
-@app.route("/cars", methods=['GET'])
+@app.route("/api/cars", methods=['GET'])
 def fetchAllCars():
 	# results = list(mongo.db.cars.find({}))
 	# return json.dumps(results, default=json_util.default)
@@ -77,58 +70,11 @@ def fetchAllCars():
 # 	results = list(mongo.db.cars.find({"make": make}))
 # 	return json.dumps(results, default=json_util.default)
 
-# @app.route("/refreshCars", methods=['GET'])
-# def refreshCars():
-
-# 	mongo.db.cars.delete_many({})
-
-# 	mongo.db.cars.insert_many([
-# 		{
-# 			"make":"Honda",
-# 			"model":"CR-V",
-# 			"year":"2005", 
-# 			"bodyType":"Luxury Sports SUV/Supercar"
-# 		},
-# 		{
-# 			"make":"Toyota",
-# 			"model":"RAV4",
-# 			"year":"2018", 
-# 			"bodyType":"Large Coupe"
-# 		},
-# 		{
-# 			"make":"Tesla",
-# 			"model":"Model S",
-# 			"year":"2017", 
-# 			"bodyType":"Electric"
-# 		},
-# 	])
-# 	return "done!"
-
-# @app.route("/refreshQuestions", methods=['GET'])
-# def refreshQuestions():
-
-# 	mongo.db.questions.delete_many({})
-
-# 	mongo.db.questions.insert_many([
-# 		{
-# 			"id": "1",
-# 			"question": "Would you want a new or used car?",
-# 			"choices": ["New", "Used"]
-# 		},
-# 		{
-# 			"id": "2",
-# 			"question": "What is your budget?",
-# 			"choices": []
-# 		}
-# 	])
-# 	return "done!"
-
-@app.route("/kenny")
-def kenny():
-	return "This is kenny's profile page."
 
 # Instructions on how to launch our Flask server: 
 # Type into Terminal:  python3 app.py
+
+# TODO: change below host='0.0.0.0' to be production-ready.
 
 if __name__ == '__main__':
 	app.run(host='0.0.0.0')
--- readme.md
+++ readme.md
@@ -1,23 +1,7 @@
-# To do: 
-
-- on front end: 
-  - import jquery
-  - add a "form" which lets you POST
-  - write jquery code that takes JSON response and edits DOM (html page) depending on response.
-
-- on back end: 
-  - open API to receive POST
-  - process logic upon receiving POST
-  - return next question JSON to front end
-  - OR return final answer jSON to front end.
-
-# To run: 
-- start mongodb with `mongod`
-- run `python3 app.py`
 
 # To run with Docker: 
-`docker build -t car-survey-api`  
+`docker build -t $USERNAME/car-survey-api`  
 
-`docker run -p 5000:5000 car-survey-api`  
+`docker run -p 5000:5000 $USERNAME/car-survey-api`  
 
 Then navigate to 0.0.0.0:5000  
